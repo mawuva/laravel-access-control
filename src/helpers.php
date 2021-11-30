b609c838286e800e7c877ccc12903bb9cf060257
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-30 16:27:58 +0100

    Feat: Add functions to manage uuids in helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,45 @@
 <?php
+
+if (!function_exists('uuid_is_enabled_and_has_column_defined')) {
+    /**
+     * Check if uuid is enabled and has column defined in config.
+     * 
+     * @return bool
+     */
+    function uuid_is_enabled_and_has_column_defined(): bool {
+        $uuidEnabled    = config('custom-user.uuids.enabled');
+        $uuidColumn     = config('custom-user.uuids.column');
+
+        return ($uuidEnabled && $uuidColumn !== null)
+                ? true
+                : false;
+    }
+}
+
+if (!function_exists('resolve_key')) {
+    /**
+     * Get key to use to make queries
+     * 
+     * @param string $config
+     * @param string $entity
+     * @param int|string $id
+     * @param bool $inTrashed
+     * 
+     * @return string|null
+     */
+    function resolve_key(string $config, string $entity, $id = null, $inTrashed = false) {
+        $model          = config($config.'.'.$entity.'.model');
+        $uuidColumn     = config($config.'.uuids.column');
+        $entityPK       = config($config.'.'.$entity.'.table.primary_key');
+
+        if (config($config.'.uuids.enabled')) {
+            return (is_the_given_id_a_uuid($uuidColumn, $id, $model, $inTrashed))
+                    ? $uuidColumn
+                    : $entityPK;
+        }
+
+        else {
+            return $entityPK;
+        }   
+    }
+}
