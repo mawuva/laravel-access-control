17a745669db93d7b55aad2706582bff5e88eb5c3
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-30 16:25:32 +0100

    Feat: Provides Accontroll class, with methods to deal with entities queries

--- src/Accontrol.php
+++ src/Accontrol.php
@@ -2,7 +2,44 @@
 
 namespace Mawuekom\Accontrol;
 
+use Illuminate\Database\Eloquent\Model;
+
 class Accontrol
 {
-    // Build your next great package.
+    /**
+     * Get entity resource by field and value
+     * 
+     * @param string $entity_slug
+     * @param string $attribute
+     * @param string $value
+     * @param bool   $inTrashed
+     * @param array  $columns
+     * 
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getEntityByField(string $entity_slug, $field, $value = null, $inTrashed = false, $columns = ['*']): Model
+    {
+        $data = app(config('accontrol.'.$entity_slug.'.model')) ->where($field, '=', $value);
+
+        return ($inTrashed)
+                    ? $data ->withTrashed() ->first($columns)
+                    : $data ->first($columns);
+    }
+
+    /**
+     * Get entity resource by id
+     * 
+     * @param string        $entity_slug
+     * @param int|string    $id
+     * @param bool          $inTrashed
+     * @param array         $columns
+     * 
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getEntityById(string $entity_slug, $id, $inTrashed = false, $columns = ['*'])
+    {
+        $key = resolve_key('accontrol', config('accontrol.'.$entity_slug.'.slug'), $id, $inTrashed);
+
+        return $this ->getEntityByField($entity_slug, $key, $id, $inTrashed, $columns);
+    }
 }
