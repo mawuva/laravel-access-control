34f4614ae0fc1d77f4bc08728e24ed0ef1d62618
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-30 15:53:31 +0100

    Refactor: Add roles and role_user tables config

--- config/accontrol.php
+++ config/accontrol.php
@@ -15,6 +15,40 @@ return [
     'separator' => env('ACCONTROL_DEFAULT_SEPARATOR', '.'),
 
     /*
+    | Manage role config
+    */
+    'role'          => [
+        'enabled'           => env('ACCONTROL_ROLES_ENABLE', true),
+        'name'              => env('ACCONTROL_ROLES_LABEL', 'Role'),
+        'slug'                => env('ACCONTROL_ROLES_SLUG', 'role'),
+        'model'             => env('ACCONTROL_ROLES_MODEL', Mawuekom\Accontrol\Models\Role::class),
+
+        /*
+        | The name of the parameter you set in your web.php or api.php to get role's ID
+        */
+        'id_route_param'    => 'id',
+
+        'table'             => [
+            'name'          => env('ACCONTROL_ROLES_DATABASE_TABLE', 'roles'),
+            'primary_key'   => env('ACCONTROL_ROLES_DATABASE_TABLE_PRIMARY_KEY', 'id'),
+        ],
+    ],
+
+    'role_user'     => [
+        'enable'                    => true,
+        'name'                      => env('ACCONTROL_ROLE_USER_LABEL', 'Role User'),
+        'slug'                      => env('ACCONTROL_ROLE_USER_SLUG', 'role_user'),
+        'model'                     => env('ACCONTROL_ROLE_USER_MODEL', null),
+
+        'table'                     => [
+            'name'                  => env('ACCONTROL_ROLE_USER_DATABASE_TABLE', 'role_user'),
+            'primary_key'           => env('ACCONTROL_ROLE_USER_DATABASE_TABLE_PRIMARY_KEY', 'id'),
+            'role_foreign_key'      => env('ACCONTROL_ROLE_USER_DATABASE_TABLE_ROLE_FOREIGN_KEY', 'role_id'),
+            'user_foreign_key'      => env('ACCONTROL_ROLE_USER_DATABASE_TABLE_USER_FOREIGN_KEY', 'user_id'),
+        ],
+    ],
+
+    /*
     |--------------------------------------------------------------------------
     | Inheritance
     |--------------------------------------------------------------------------
