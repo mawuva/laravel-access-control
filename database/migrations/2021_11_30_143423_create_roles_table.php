e7223205dd623a38e8f16d1a64194b6408f69946
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-30 15:54:01 +0100

    Feat: Add CreateRolesTable migration

---
+++ database/migrations/2021_11_30_143423_create_roles_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $uuidEnabled     = config('accontrol.uuids.enabled');
+        $uuidColumn      = config('accontrol.uuids.column');
+
+        $roleEnabled     = config('accontrol.role.enabled');
+        $rolesTable      = config('accontrol.role.table.name');
+        $rolesTablePK    = config('accontrol.role.table.primary_key');
+        
+        if ($roleEnabled) {
+            Schema::create($rolesTable, function (Blueprint $table) 
+            use ($rolesTablePK, $uuidEnabled, $uuidColumn) {
+                $table->id($rolesTablePK);
+
+                if ($uuidEnabled && $uuidColumn !== null) {
+                    $table->uuid($uuidColumn);
+                }
+                
+                $table->string('name');
+                $table->string('slug')->unique();
+                $table->longText('description')->nullable();
+                $table->integer('level')->default(1);
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $rolesTable  = config('accontrol.role.table.name');
+
+        Schema::dropIfExists($rolesTable);
+    }
+}
