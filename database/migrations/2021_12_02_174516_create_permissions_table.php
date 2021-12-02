39bdfe3363a5884ff3617db47c879eb01f62297e
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-02 21:22:19 +0100

    Feat: Add CreatePermissionsTable migration

---
+++ database/migrations/2021_12_02_174516_create_permissions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $uuidEnabled            = config('accontrol.uuids.enabled');
+        $uuidColumn             = config('accontrol.uuids.column');
+
+        $permissionEnabled      = config('accontrol.permission.enabled');
+        $permissionsTable       = config('accontrol.permission.table.name');
+        $permissionsTablePK     = config('accontrol.permission.table.primary_key');
+
+        if ($permissionEnabled) {
+            Schema::create($permissionsTable, function (Blueprint $table) 
+            use ($permissionsTablePK, $uuidEnabled, $uuidColumn) {
+                $table->id($permissionsTablePK);
+
+                if ($uuidEnabled && $uuidColumn !== null) {
+                    $table->uuid($uuidColumn);
+                }
+                
+                $table->string('name');
+                $table->string('slug')->unique();
+                $table->longText('description')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $permissionsTable  = config('accontrol.permission.table.name');
+
+        Schema::dropIfExists($permissionsTable);
+    }
+}
