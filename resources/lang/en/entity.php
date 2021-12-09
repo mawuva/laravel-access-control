9ff13d99eeca5b46dc1e7382ceba12740f9cdd79
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-09 23:59:19 +0100

    Refactor: Add new languages resources entries for denied cases

--- resources/lang/en/entity.php
+++ resources/lang/en/entity.php
@@ -1,8 +1,14 @@
 <?php
 
 return [
-    'action'                    => 'Action|Actions',
-    'entity'                    => 'Entity|Entities',
-    'permission'                => 'Permission|Permissions',
-    'role'                      => 'Role|Roles',
+    'action'        => 'Action|Actions',
+    'entity'        => 'Entity|Entities',
+    'permission'    => 'Permission|Permissions',
+    'role'          => 'Role|Roles',
+
+    'messages'      => [
+        'do_not_have_required_level'        => "You don't have a required [%s] level.",
+        'do_not_have_required_permission'   => "You don't have a required ['%s'] permission.",
+        'do_not_have_required_role'         => "You don't have a required ['%s'] role.",
+    ],
 ];
--- resources/lang/fr/entity.php
+++ resources/lang/fr/entity.php
@@ -5,4 +5,10 @@ return [
     'entity' => 'Entité|Entités',
     'permission' => 'Permission|Permissions',
     'role' => 'Rôle|Rôles',
+
+    'messages'      => [
+        'do_not_have_required_level'        => "Vous n'avez pas le niveau [%s] qui est requis.",
+        'do_not_have_required_permission'   => "Vous n'avez la permission ['%s'] qui est requise.",
+        'do_not_have_required_role'         => "Vous n'avez pas le role ['%s'] qui est requis.",
+    ],
 ];
