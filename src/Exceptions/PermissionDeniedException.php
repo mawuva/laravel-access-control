5773e84ce9eaf96ad6c498aff03245b6f25ca0bf
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-09 23:59:39 +0100

    Feat: Add Denied exceptions

---
+++ src/Exceptions/PermissionDeniedException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mawuekom\Accontrol\Exceptions;
+
+use Mawuekom\Accontrol\Exceptions\AccessDeniedException;
+
+class PermissionDeniedException extends AccessDeniedException
+{
+    /**
+     * Create a new permission denied exception instance.
+     *
+     * @param string $permission
+     */
+    public function __construct($permission)
+    {
+        $this->message = sprintf(trans('accontrol::entity.messages.do_not_have_required_permission'), $permission);
+    }
+}
