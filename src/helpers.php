0183d42cd96d6f0857bcd76fe5df8d56fd04afa7
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-10 01:15:15 +0100

    Feat: Add functions in helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -16,3 +16,97 @@ if (!function_exists('uuid_is_enabled_and_has_column_defined')) {
     }
 }
 
+if (!function_exists('has_level')) {
+    /**
+     * Check if user is authenticated and has given level
+     * 
+     * @return bool
+     */
+    function has_level($level): bool {
+        if (auth() || auth('admin')) {
+            $user = (auth('admin') ->user() !== null) ? auth('admin') ->user() : auth() ->user();
+
+            if ($user()->level() >= $level) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('has_permission')) {
+    /**
+     * Check if user is authenticated and has given permission
+     * 
+     * @return bool
+     */
+    function has_permission($permission): bool {
+        if (auth() || auth('admin')) {
+            $user = (auth('admin') ->user() !== null) ? auth('admin') ->user() : auth() ->user();
+
+            if (str_contains($permission, '|')) {
+                $permissions = explode('|', $permission);
+
+                for ($i = 0; $i < count($permissions); $i++) {
+                    if ($user()->hasRole($permissions[$i])) {
+                        return true;
+                    }
+                }
+            }
+
+            else {
+                if ($user() ->hasPermission($permission)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('has_role')) {
+    /**
+     * Check if user is authenticated and has given role
+     * 
+     * @return bool
+     */
+    function has_role($role): bool {
+        if (auth() || auth('admin')) {
+            $user = (auth('admin') ->user() !== null) ? auth('admin') ->user() : auth() ->user();
+
+            if (str_contains($role, '|')) {
+                $roles = explode('|', $role);
+
+                for ($i = 0; $i < count($roles); $i++) {
+                    if ($user()->hasRole($roles[$i])) {
+                        return true;
+                    }
+                }
+            }
+
+            elseif (is_array($role)) {
+                for ($i = 0; $i < count($role); $i++) {
+                    if ($user()->hasRole($role[$i])) {
+                        return true;
+                    }
+                }
+            }
+
+            else {
+                if ($user()->hasRole($role)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
