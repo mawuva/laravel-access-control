9064ff2854969f8442285c46ce38e6de0a0c3499
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-14 00:23:20 +0100

    Feat(seeders): Add seeders for actions and entities tables

---
+++ database/seeders/DefaultActionsTableSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Mawuekom\Accontrol\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Mawuekom\Accontrol\Events\ActionWasCreated;
+
+class DefaultActionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+         * Action Types
+         *
+         */
+        $actionItems = [
+            [
+                'name'                          => 'Create',
+                'slug'                          => 'create',
+                'description'                   => 'Create new data',
+                'available_for_all_entities'    => 1,
+            ],
+            [
+                'name'                          => 'View',
+                'slug'                          => 'view',
+                'description'                   => 'View created data',
+                'available_for_all_entities'    => 1,
+            ],
+            [
+                'name'                          => 'Edit',
+                'slug'                          => 'edit',
+                'description'                   => 'Edit created data',
+                'available_for_all_entities'    => 1,
+            ],
+            [
+                'name'                          => 'Delete',
+                'slug'                          => 'delete',
+                'description'                   => 'Delete created data',
+                'available_for_all_entities'    => 1,
+            ],
+            [
+                'name'                          => 'Restore',
+                'slug'                          => 'restore',
+                'description'                   => 'Restore deleted data',
+                'available_for_all_entities'    => 1,
+            ],
+        ];
+
+        /*
+         * Add action Items
+         *
+         */
+        echo "\e[32mSeeding:\e[0m Accontrol - DefaultActionItemsTableSeeder\r\n";
+        
+        foreach ($actionItems as $actionItem) {
+            $newActionItem = config('accontrol.action.model')::where('slug', '=', $actionItem['slug'])->first();
+
+            if ($newActionItem === null) {
+                $newActionItem = config('accontrol.action.model')::create([
+                    'name'                          => $actionItem['name'],
+                    'slug'                          => $actionItem['slug'],
+                    'description'                   => $actionItem['description'],
+                    'available_for_all_entities'    => $actionItem['available_for_all_entities'],
+                ]);
+
+                event(new ActionWasCreated($newActionItem));
+                
+                echo "\e[32mSeeding:\e[0m Accontrol - DefaultActionItemsTableSeeder - Action:".$actionItem['slug']."\r\n";
+            }
+        }
+    }
+}
---
+++ database/seeders/DefaultEntitiesTableSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Mawuekom\Accontrol\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Mawuekom\Accontrol\Events\EntityWasCreated;
+
+class DefaultEntitiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+         * Entity Types
+         *
+         */
+        $entityItems = [
+            [
+                'name'          => 'Action',
+                'slug'          => 'action',
+                'model'         => config('accontrol.action.model'),
+                'description'   => 'Operations that are done on an entity.',
+            ],
+            [
+                'name'          => 'Entity',
+                'slug'          => 'entity',
+                'model'         => config('accontrol.entity.model'),
+                'description'   => 'Operations that are done on an entity.',
+            ],
+            [
+                'name'          => 'Permission',
+                'slug'          => 'permission',
+                'model'         => config('accontrol.permission.model'),
+                'description'   => 'Authorization which are granted to users.',
+            ],
+            [
+                'name'          => 'Role',
+                'slug'          => 'role',
+                'model'         => config('accontrol.role.model'),
+                'description'   => 'Roles are a group of permissions or roles.',
+            ],
+            [
+                'name'          => 'User',
+                'slug'          => 'user',
+                'model'         => App\Models\User::class,
+                'description'   => 'Users who run the app.',
+            ],
+        ];
+
+        /*
+         * Add entity Items
+         *
+         */
+        echo "\e[32mSeeding:\e[0m Accontrol - DefaultEntityItemsTableSeeder\r\n";
+        
+        foreach ($entityItems as $entityItem) {
+            $newEntityItem = config('accontrol.entity.model')::where('slug', '=', $entityItem['slug'])->first();
+
+            if ($newEntityItem === null) {
+                $newEntityItem = config('accontrol.entity.model')::create([
+                    'name'          => $entityItem['name'],
+                    'slug'          => $entityItem['slug'],
+                    'model'         => $entityItem['model'],
+                    'description'   => $entityItem['description'],
+                ]);
+
+                event(new EntityWasCreated($newEntityItem));
+                
+                echo "\e[32mSeeding:\e[0m Accontrol - DefaultEntityItemsTableSeeder - Entity:".$entityItem['slug']."\r\n";
+            }
+        }
+    }
+}
